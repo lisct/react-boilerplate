de8f8d63b7590dc5b8332d31af0f241e4c2fa7c0
Lisbel Cruz <lis@padsquad.com>
2018-04-29 17:57:45 -0500

    Adding webpack devtool fast. (rebuild)

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = (env) => {
     plugins: [
       CSSExtract
     ],
-    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
